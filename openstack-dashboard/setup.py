5207deb01d9d3113dc1f736386a34a42a4206949
Sergey Kosyrev <skosyrev@griddynamics.com>
2011-09-28 18:03:21 +0400

    Put correct setup script

--- openstack-dashboard/setup.py
+++ openstack-dashboard/setup.py
@@ -18,40 +18,27 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+
+#!/usr/bin/env python
 import os
-import shutil
 from setuptools import setup, find_packages, findall
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-dst = 'debian/openstack-dashboard/var/lib/dash'
-os.system('rm -rf %s' % dst)
-shutil.copytree('tools', '%s/tools' % dst)
-shutil.copytree('dashboard', '%s/dashboard' % dst)
-shutil.copytree('local', '%s/local' % dst)
-
-
 setup(
-    name = "openstack-dashboard",
-    version = "0.2",
-    url = 'https://github.com/cloudbuilders/openstack-dashboard.git',
+    name = "openstack_dashboard",
+    version = "1.0",
+    url = 'https://launchpad.net/openstack-dashboard',
     license = 'Apache 2.0',
-    description = "A Django interface for OpenStack.",
-    long_description = read('README'),
-    author = 'Devin Carlen',
-    author_email = 'devin.carlen@gmail.com',
-#    packages = find_packages(),
-#    package_data = {'openstack-dashboard':
-#                        [s[len('dashboard/'):] for s in
-#                         findall('dashboard/templates')]},
-
+    description = "Django based reference implementation of a web based management interface for OpenStack.",
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "local"]),
+    package_data = {'openstack_dashboard':
+                        [s[len('openstack_dashboard/'):] for s in
+                        findall('openstack_dashboard/templates') +
+                        findall('openstack_dashboard/wsgi') +
+                        findall('openstack_dashboard/locale') +
+                        findall('openstack_dashboard/static')],
+                    },
     data_files = [('/etc/openstack_dashboard/local', findall('local')), ('/var/lib/openstack_dashboard', set())],
-    install_requires = ['setuptools', 'mox>=0.5.0'],
-    zip_safe = False,
     classifiers = [
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache License',
         'Operating System :: OS Independent',
